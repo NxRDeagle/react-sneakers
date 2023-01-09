65db35b570534aed0abf994165cbb5cc18f52404
NordEagle <91448036+NordEagle@users.noreply.github.com>
2023-01-10 00:46:46 +0500

    react hooks

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,21 +1,23 @@
-function Drawer() {
+function Drawer({ onCloseCart, items = [] }) {
     return (
-        <div style={{ display: 'none' }} className="overlay">
+        <div className="overlay">
             <div className="drawer">
                 <h2 className="d-flex justify-between mb-30">Корзина
-                    <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Remove" />
+                    <img className="removeBtn cu-p" onClick={onCloseCart} src="/img/btn-remove.svg" alt="Close" />
                 </h2>
 
-                <div className="cartItem d-flex align-center mb-20">
-                    <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg"></div>
-                    <div className="mr-20 flex">
-                        <p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
-                        <b>12 999 руб.</b>
-                    </div>
-                    <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-                </div>
-
                 <div className="Items">
+                    {items.map((obj) => (
+                        <div className="cartItem d-flex align-center mb-20">
+                            <div style={{ backgroundImage: `url(${obj.imageUrl})` }} className="cartItemImg"></div>
+                            <div className="mr-20 flex">
+                                <p className="mb-5">{obj.title}</p>
+                                <b>{obj.price} руб.</b>
+                            </div>
+                            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+                        </div>
+                    ))}
+
                     <div className="cartTotalBlock">
                         <ul>
                             <li className="d-flex">
