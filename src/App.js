58e7548a3647aa9ba31cd40f0bb321209188c413
NordEagle <91448036+NordEagle@users.noreply.github.com>
2023-01-15 12:10:31 +0500

    react route

--- src/App.js
+++ src/App.js
@@ -1,71 +1,87 @@
 import React from 'react';
-import Card from './components/Card';
+import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import Header from './components/Header.js';
 import Drawer from './components/Drawer.js';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
+  const [favorites, setFavorites] = React.useState([]);
   const [searchValue, setSearchValue] = React.useState('');
-  const [cartIsOpened, setCartOpened] = React.useState(false);
+  const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    fetch("https://63bc4615fa38d30d85c2bceb.mockapi.io/items")
-      .then((res) => res.json())
-      .then((json) => setItems(json))
+    axios.get('https://63bc4615fa38d30d85c2bceb.mockapi.io/items').then((res) => {
+      setItems(res.data);
+    });
+    axios.get('https://63bc4615fa38d30d85c2bceb.mockapi.io/cart').then((res) => {
+      setCartItems(res.data);
+    });
+    axios.get('https://63c3a57da9085635752ac471.mockapi.io/favorites').then((res) => {
+      setFavorites(res.data);
+    });
   }, []);
 
   const onAddToCart = (obj) => {
-    setCartItems((prev) => {
-      if (!cartItems.find(element => element.imageUrl === obj.imageUrl))
-        return [...prev, obj];
-      return [...prev];
-    });
-  }
+    axios.post('https://63bc4615fa38d30d85c2bceb.mockapi.io/cart', obj);
+    setCartItems((prev) => [...prev, obj]);
+  };
+
+  const onRemoveItem = (id) => {
+    axios.delete(`https://63bc4615fa38d30d85c2bceb.mockapi.io/cart/${id}`);
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const onAddToFavorite = async (obj) => {
+    try {
+      if (favorites.find((favObj) => favObj.id === obj.id)) {
+        axios.delete(`https://63c3a57da9085635752ac471.mockapi.io/favorites/${obj.id}`);
+      } else {
+        const { data } = await axios.post('https://63c3a57da9085635752ac471.mockapi.io/favorites', obj);
+        setFavorites((prev) => [...prev, data]);
+      }
+    } catch (error) {
+      alert('Не удалось добавить в фавориты');
+    }
+  };
 
   const onChangeSearchInput = (event) => {
     setSearchValue(event.target.value);
-  }
+  };
 
   return (
     <div className="wrapper clear">
-      {cartIsOpened &&
-        <Drawer
-          onCloseCart={() => { setCartOpened(false) }}
-          items={cartItems}
-        />}
-      <Header onClickCart={() => { setCartOpened(true) }} />
+      {cartOpened && (
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />
+      )}
 
-      <div className="content p-40">
-        <div className="d-flex align-center justify-between mb-40">
-          <h1>{searchValue ? `Поиск по запросу "${searchValue}"` : "Все кроссовки"}</h1>
-          <div className="searchBlock">
-            <img src="/img/search.svg" alt="Search" />
-            <input
-              placeholder="Поиск..."
-              onChange={onChangeSearchInput}
+      <Header onClickCart={() => setCartOpened(true)} />
+
+      <Routes>
+        <Route path="/" exact
+          element=
+          {
+            <Home
+              items={items}
+              searchValue={searchValue}
+              setSearchValue={setSearchValue}
+              onChangeSearchInput={onChangeSearchInput}
+              onAddToFavorite={onAddToFavorite}
+              onAddToCart={onAddToCart}
             />
-            {
-              searchValue && <img className="clear cu-p" src="/img/btn-remove.svg" alt="Clear" />
-            }
-          </div>
-        </div>
+          }
+        />
 
-        <div className="d-flex flex-wrap">
-          {items.filter(item => item.title.toLowerCase().includes(searchValue.toLowerCase()))
-          .map((item, index) => (
-            <Card
-              key={index}
-              title={item.title}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              onFavorite={() => console.log("Добавили в закладки")}
-              onPlus={(obj) => onAddToCart(obj)}
-            />
-          ))}
-        </div>
-
-      </div>
+        <Route path="/favorites" exact
+          element=
+          {
+            <Favorites items={favorites} onAddToFavorite={onAddToFavorite} />
+          }
+        />
+      </Routes>
     </div>
   );
 }
